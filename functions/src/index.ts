29037fdf03320bbaae4464c12213cc36ed0bc596
Rei-Sogawa <natto.and.eggs@gmail.com>
2021-10-11 22:05:08 +0900

    a

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 import { firestore } from 'firebase-admin'
 import * as functions from 'firebase-functions'
-import { _User } from 'shared'
+import { HELLO_WORLD } from 'shared'
 
 const functionsWithRegion = functions.region('asia-northeast1')
 
 export const helloWorld = functionsWithRegion.https.onRequest((request, response) => {
-  response.send(_User.defaultData({ now: firestore.Timestamp.now() }).createdAt)
+  response.send(HELLO_WORLD)
 })
